53248139ad3e7007fcd9aa5f5833c94247af6985
Caleb <calebharper77@gmail.com>
2024-12-08 16:58:56 -0600

    Initial Commit

---
+++ CustomIcons/CustomIconsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomIconsApp.swift
+//  CustomIcons
+//
+//  Created by Caleb Harper on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomIconsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CustomIconsTests/CustomIconsTests.swift
@@ -0,0 +1,16 @@
+//
+//  CustomIconsTests.swift
+//  CustomIconsTests
+//
+//  Created by Caleb Harper on 12/8/24.
+//
+
+import Testing
+
+struct CustomIconsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
